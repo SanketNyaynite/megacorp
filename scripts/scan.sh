3166980a6a3f932c3552b7a3787ba3c9a6614f2b
SanketNyaynite <sanket.nyaynite@gmail.com>
2024-09-25 15:23:39 -0500

    L: added

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,4 +1,4 @@
-echo "\n====== SCANNING FOR CREDIT CARD NUMBERS ======"
+echo "\n====== SCANNING FOR CREDIT CARD NUMBERS ====="
 grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
 echo "========= CREDIT CARD SCAN COMPLETE =========="
 echo "\n==== SCANNING FOR SOCIAL SECURITY NUMBERS ===="
